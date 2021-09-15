13f27ed013d7da09f8c78ec94d7ee43539509afd
crptbosyak <75790510+crptbosyak@users.noreply.github.com>
2021-09-15 19:22:27 +0200

    Update MenuController.java

--- src/menu/MenuController.java
+++ src/menu/MenuController.java
@@ -7,18 +7,14 @@ import java.util.Scanner;
 
 public class MenuController {
     public void run() {
-
         Builder builder = new Builder();
         builder.buildMainMenu();
         Navigator navigator = new Navigator(builder.getRootMenu());
-
         do {
             navigator.printMenu();
             Scanner scanner = new Scanner(System.in);
             int select = scanner.nextInt();
             navigator.navigate(select);
-
         } while (navigator.getCurrentMenu() != null);
-
     }
 }
